202eb842c004f7ab813bf75e086d696178134df6
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-05-21 01:55:11 +0600

    successfully added deleted funciton

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -6,10 +6,10 @@ const Details = () => {
     const data = useLoaderData()
     console.log(data)
     return (
-        <div className='grid grid-cols-3 gap-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
             
             {
-                data.map(detail=><DetailsCard detail={detail}></DetailsCard>)
+                data.map(detail=><DetailsCard key={detail._id} detail={detail}></DetailsCard>)
             }
         </div>
     );
--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const DetailsCard = ({detail}) => {
     console.log(detail)
+
+    const handleDelete =(id)=>{
+        Swal.fire({
+  title: `Are you sure? you want to delete id: ${id}`,
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+    fetch(`http://localhost:3000/add-roommate/${id}`,{
+        method:'DELETE'
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log('after delete', data)
+        if(data.deletedCount>0){
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+        }
+    })
+
+  }
+});
+    }
     return (
         <div className="card bg-neutral text-neutral-content">
             <div className="card-body items-center text-center">
@@ -9,7 +41,7 @@ const DetailsCard = ({detail}) => {
                 <p>We are using cookies for no reason.</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Edit</button>
-                    <button className="btn btn-ghost">Delete</button>
+                    <button onClick={()=>handleDelete(detail._id)} className="btn btn-ghost">Delete</button>
                 </div>
             </div>
         </div>
