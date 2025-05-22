e7b9f8656f5072605b70f2a663e6b0519a568cb6
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-05-22 15:17:03 +0600

    added featured roommates post section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../components/Banner';
+import FeaturedRoommates from '../components/FeaturedRoommates';
 
 const Home = () => {
+    const [posts, setPosts] = useState([])  
+    
+useEffect(()=>{
+        fetch('http://localhost:3000/add-roommate')
+    .then(res=>res.json())
+    .then(data=>{
+     setPosts(data)
+    //  console.log(data)
+    })
+},[])
+console.log(posts)
     return (
         <div>
             <Banner></Banner>
+                    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+            {
+                posts.map(post=><FeaturedRoommates key={post._id} post={post}></FeaturedRoommates>)
+            }
+
+
+            </div>
         </div>
+            
+        </div>
+
+  
     );
 };
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,62 +3,93 @@ import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 const Banner = () => {
-    const onChange = (index) => {
-        // console.log(`Slide changed to ${index}`);
-    };
 
-    const onClickItem = (index) => {
-        // console.log(`Clicked item ${index}`);
-    };
 
-    const onClickThumb = (index) => {
-        // console.log(`Clicked thumb ${index}`);
-    };
+
+    // const onChange = (index) => {
+    //     console.log(`Slide changed to ${index}`);
+    // };
+
+    // const onClickItem = (index) => {
+    //     console.log(`Clicked item ${index}`);
+    // };
+
+    // const onClickThumb = (index) => {
+    //     console.log(`Clicked thumb ${index}`);
+    // };
     return (
         <Carousel
             showArrows={true}
-            onChange={onChange}
-            onClickItem={onClickItem}
-            onClickThumb={onClickThumb}
+            // onChange={onChange}
+            // onClickItem={onClickItem}
+            // onClickThumb={onClickThumb}
             autoPlay={true}
             interval={2000}
             infiniteLoop={true}
             showThumbs={false}
             showStatus={false}>
-           
-            
 
-            <div className='bg-[url(https://i.ibb.co/6JbzQr3q/Awesome-Green-Nature-Wallpaper-HD.jpg)] h-screen bg-cover'>
 
-            <div className="card card-side shadow-sm pt-22">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      alt="Movie" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
-            <h2 className='pt-12 text-4xl font-bold text-white'>tis is on e</h2>
+
+            <div className='bg-[url(https://i.ibb.co/6JbzQr3q/Awesome-Green-Nature-Wallpaper-HD.jpg)]  bg-cover'>
+
+                <div className="card card-side shadow-sm pt-22">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New movie is released!</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Watch</button>
+                        </div>
+                    </div>
+                </div>
+                <h2 className='pt-12 text-4xl font-bold text-white'>tis is on e</h2>
                 {/* <img src="https://i.ibb.co/Hpkr1dF4/ad01682307049.jpg" /> */}
                 <p className="legend">Legend 4</p>
             </div>
-            <div className='bg-[url(https://i.ibb.co/PsWq8tC6/ad06964907556.jpg)] w-full h-screen bg-cover'>
-            <h2 className='pt-12 text-4xl font-bold text-white'>tis is on e</h2>
+            <div className='bg-[url(https://i.ibb.co/PsWq8tC6/ad06964907556.jpg)] w-full bg-cover'>
+                            <div className="card card-side shadow-sm pt-22">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New movie is released!</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Watch</button>
+                        </div>
+                    </div>
+                </div>
+                <h2 className='pt-12 text-4xl font-bold text-white'>tis is on e</h2>
                 {/* <img src="https://i.ibb.co/Hpkr1dF4/ad01682307049.jpg" /> */}
                 <p className="legend">Legend 4</p>
             </div>
-            <div className='bg-[url(https://i.ibb.co/Hpkr1dF4/ad01682307049.jpg)] w-full h-screen bg-cover'>
-            <h2 className='pt-12 text-4xl font-bold text-white'>tis is on e</h2>
+            <div className='bg-[url(https://i.ibb.co/Hpkr1dF4/ad01682307049.jpg)] w-full bg-cover'>
+                            <div className="card card-side shadow-sm pt-22">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New movie is released!</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Watch</button>
+                        </div>
+                    </div>
+                </div>
+                <h2 className='pt-12 text-4xl font-bold text-white'>tis is on e</h2>
                 {/* <img src="https://i.ibb.co/Hpkr1dF4/ad01682307049.jpg" /> */}
                 <p className="legend">Legend 4</p>
             </div>
-            
+
         </Carousel>
     );
 };
