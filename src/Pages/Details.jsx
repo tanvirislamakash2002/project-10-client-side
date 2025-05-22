0f47ded4fc78a55149a0f65f1897075d5a457aa1
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-05-22 18:51:59 +0600

    successfully added like option

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,72 +1,114 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import DetailsCard from '../components/DetailsCard';
 import { AuthContext } from '../provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const Details = () => {
-    const {user} = use(AuthContext)
+    const { user } = use(AuthContext)
     const data = useLoaderData()
-    const {id} = useParams()
-    const singleData = data.find(info=>info._id==id)
-    const {_id, availability, contact_info, description, location, post_email, post_name, post_user_photo, preferences, rent_amount, roomType} = singleData
-    console.log('this si', singleData)
+    const { id } = useParams()
+
+    
+    const singleData = data.find(info => info._id == id)
+    const { _id, availability, contact_info, description, location, post_email, post_name, post_user_photo, preferences, rent_amount, roomType, liked_by } = singleData
+    
+    console.log(liked_by)
+    
+    // like management 
+    const [liked, setLiked] = useState(false)
+    const checkAvailable = liked_by.includes(user.email)
+    // checkAvailable&&setLiked(false)
+    console.log(checkAvailable)
+
+    const handleLiked=()=>{
+        
+        setLiked(!liked)
+const filterAvailable = liked_by.filter(filterEmail=>filterEmail!=user.email)
+        const updateLike =checkAvailable?{liked_by:filterAvailable}:{ liked_by:[...liked_by, user.email]}
+        console.log('filtered',filterAvailable)
+    
+        fetch(`http://localhost:3000/add-roommate/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateLike)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Updated successfully",
+                        showConfirmButton: true,
+                        timer: 900
+                    });
+                }
+            })
+    }
+
+
+
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 
             <div className="card w-96 bg-base-100 shadow-sm">
-  <div className="card-body">
-    <span className="badge badge-xs badge-warning">Most Popular</span>
-    <div className="flex justify-between">
-      <h2 className="text-3xl font-bold">Premium</h2>
-      <img src={post_user_photo} alt="" />
-      <span className="text-xl">$29/mo</span>
-    </div>
-    <ul className="mt-6 flex flex-col gap-2 text-xs">
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{location}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{contact_info}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{rent_amount}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{availability}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{description}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{post_email}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{post_name}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{preferences}</span>
-      </li>
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>{roomType}</span>
-      </li>
+                <div className="card-body">
+                    <span className="badge badge-xs badge-warning">Most Popular</span>
+                    <div className="flex justify-between">
+                        <h2 className="text-3xl font-bold">Premium</h2>
+                        <img src={post_user_photo} alt="" />
+                        <span className="text-xl">$29/mo</span>
+                    </div>
+                    <ul className="mt-6 flex flex-col gap-2 text-xs">
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{location}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{contact_info}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{rent_amount}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{availability}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{description}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{post_email}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{post_name}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{preferences}</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                            <span>{roomType}</span>
+                        </li>
 
 
-    </ul>
-    <div className="mt-6">
-      <button className="btn btn-primary btn-block">Subscribe</button>
-    </div>
-  </div>
-</div>
-            
+                    </ul>
+                    <div className="mt-6">
+                        <button onClick={handleLiked} className="btn btn-primary btn-block">{liked ? `liked` : `disliked`}</button>
+                        <p>{liked ? contact_info : ``}</p>
+                    </div>
+                </div>
+            </div>
+
             {/* {
                 data.map(detail=><DetailsCard key={detail._id} detail={detail}></DetailsCard>)
             } */}
