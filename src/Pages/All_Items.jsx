ba957b99d60e3bf96141b9c2bee7fd64232853fb
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-06-27 14:51:21 +0600

    updated all items page

--- src/Pages/All_Items.jsx
+++ src/Pages/All_Items.jsx
@@ -6,45 +6,18 @@ import { AuthContext } from '../provider/AuthProvider';
 
 const All_Items = () => {
     const data = useLoaderData()
+    // _id, post_email, post_name, post_user_photo, contact_info, location, rent_amount, description, availability
     const { darkMode } = use(AuthContext)
+
+
 
     return (
         <div className={`${darkMode && `text-white`} overflow-x-auto max-w-7xl mx-auto w-11/12`}>
             <h2 className="text-4xl font-bold text-center mt-12 mb-8">All Available Roommate Listings</h2>
-            
-            <table className="table">
-                {/* head */}
-                <thead>
-                    <tr className={`${darkMode && `text-white`}`}>
-  
-                        <th>Posted By (Name and Email)</th>
-                        <th>Location</th>
-                        <th>Rent Amount</th>
-                        <th>Description</th>
-                        <th>Availability</th>
-                        <th>Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* row  */}
 {
-    data.map(rowData=><ListingTableRow key={rowData._id} rowData={rowData}></ListingTableRow>)
+    data.map(cardData=><h2>{cardData?.location}</h2>)
 }
 
-
-
-
-                </tbody>
-                {/* foot */}
-                <tfoot>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </tfoot>
-            </table>
         </div>
     );
 };
