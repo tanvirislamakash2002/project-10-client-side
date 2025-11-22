d431570ce7b20aa3e87c7f69c6bd6745eab24d01
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-11-22 22:36:47 +0600

    chhanged thhhe logo on navbar for small device

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import logo from '../assets/text-logo.png';
+import logo1 from '../assets/text-logo.png';
+import logo2 from '../assets/symbol-logo-with-text.png';
 import { Link } from 'react-router';
 
 const Logo = () => {
     return (
         <Link to="/" className="btn btn-ghost text-xl normal-case hover:bg-transparent">
   <img 
-    src={logo} 
+    src={logo2} 
     alt="RoomEase Logo" 
-    className="h-6 w-auto transition-all duration-300 hover:filter hover:drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] dark:hover:drop-shadow-[0_2px_4px_rgba(255,255,255,0.1)]" 
+    className="h-6 w-auto transition-all duration-300 hover:filter hover:drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] dark:hover:drop-shadow-[0_2px_4px_rgba(255,255,255,0.1)] block sm:hidden" 
+  />
+  <img 
+    src={logo1} 
+    alt="RoomEase Logo" 
+    className="h-6 w-auto transition-all duration-300 hover:filter hover:drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] dark:hover:drop-shadow-[0_2px_4px_rgba(255,255,255,0.1)] hidden sm:block" 
   />
 </Link>
     );
