9c264d8e61c49825f7ac573dd5dbf2493c486c73
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-02-10 01:11:53 +0600

    added interceptor  to add  token in the header authorization

--- hooks/useAxios.jsx
+++ hooks/useAxios.jsx
@@ -5,6 +5,16 @@ import { config } from '../src/config';
 const axiosInstance = axios.create({
     baseURL: config.API_URL
 })
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token')
+        if (token) { config.headers.Authorization = `Bearer ${token}` }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
 const useAxios = () => {
     return axiosInstance;
 };
